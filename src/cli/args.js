12f2ea452276aadf6fed56712d69e56962bb28b0
Rob Marvin <fenalfan@gmail.com>
2025-10-25 19:45:18 +0300

    feat: implement parseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-  // Write your code here
+  const args = process.argv.slice(2);
+  for (const arg of args) {
+    if (arg.startsWith('--')) {
+      console.log(`${arg} is ${args[args.indexOf(arg) + 1]}`);
+    }
+  }
 };
 
 parseArgs();
