b0d49a4111525568ebfd0d1b8c0d209e011b8462
Rob Marvin <fenalfan@gmail.com>
2025-10-25 11:58:36 +0300

    feat: implement create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { errors } from '../constants/errors.js';
+
+const __dirname = import.meta.dirname;
+const filePath = path.join(__dirname, 'files', 'fresh.txt');
+
 const create = async () => {
-  // Write your code here
+  try {
+    const fd = await fs.open(filePath, 'wx');
+    await fd.writeFile('I am fresh and young');
+    await fd.close();
+  } catch {
+    throw new Error(errors.fsOperationFailed);
+  }
 };
 
 await create();
