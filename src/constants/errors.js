68a9f9e655d682f9461b38bda65f018bdedb93be
Rob Marvin <fenalfan@gmail.com>
2025-10-26 12:43:26 +0300

    feat: add write file error

--- src/constants/errors.js
+++ src/constants/errors.js
@@ -2,5 +2,6 @@ export const errors = Object.freeze({
   fsOperationFailed: 'FS operation failed',
   hashCalculationFailed: 'Hash calculation failed',
   fileReadFailed: 'Failed to read file',
+  fileWriteFailed: 'Failed to write file'
 });
 
