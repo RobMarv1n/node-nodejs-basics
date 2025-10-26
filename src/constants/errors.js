16f95b25b350e0f9fbdcad968c8c972cf3b4ef3c
Rob Marvin <fenalfan@gmail.com>
2025-10-26 13:10:46 +0300

    feat: add transform data error

--- src/constants/errors.js
+++ src/constants/errors.js
@@ -2,6 +2,7 @@ export const errors = Object.freeze({
   fsOperationFailed: 'FS operation failed',
   hashCalculationFailed: 'Hash calculation failed',
   fileReadFailed: 'Failed to read file',
-  fileWriteFailed: 'Failed to write file'
+  fileWriteFailed: 'Failed to write file',
+  transformFailed: 'Failed to transform data',
 });
 
