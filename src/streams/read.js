675eb999863f2811d176e06e42a7162818135819
Rob Marvin <fenalfan@gmail.com>
2025-10-26 12:30:46 +0300

    feat: implement read file function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,23 @@
+import { createReadStream } from 'node:fs';
+import path from 'node:path';
+import { errors } from '../constants/errors.js';
+
+const __dirname = import.meta.dirname;
+const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-  // Write your code here
+  try {
+    const stream = createReadStream(filePath);
+
+    for await (const chunk of stream) {
+      process.stdout.write(chunk);
+
+    }
+
+    process.stdout.write('\n');
+  } catch {
+    throw new Error(errors.fileReadFailed);
+  }
 };
 
 await read();
