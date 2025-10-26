d51a50365dd97d5ab0762a4f755373f8f0fb6a14
Rob Marvin <fenalfan@gmail.com>
2025-10-26 12:30:03 +0300

    feat: add file read error, prohibit changing an object with errors

--- src/constants/errors.js
+++ src/constants/errors.js
@@ -1,5 +1,6 @@
-export const errors = {
+export const errors = Object.freeze({
   fsOperationFailed: 'FS operation failed',
   hashCalculationFailed: 'Hash calculation failed',
-}
+  fileReadFailed: 'Failed to read file',
+});
 
