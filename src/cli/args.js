6eb499c97a4ac74ce36ec5b3ef53783eea7b3dfb
Rob Marvin <fenalfan@gmail.com>
2025-10-26 23:44:01 +0300

    fix: change the output of the parseArgs function according to the task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,10 +1,14 @@
 const parseArgs = () => {
   const args = process.argv.slice(2);
-  for (const arg of args) {
-    if (arg.startsWith('--')) {
-      console.log(`${arg} is ${args[args.indexOf(arg) + 1]}`);
+  const result = [];
+
+  for (let i = 0; i < args.length; i += 1) {
+    if (args[i].startsWith('--')) {
+      result.push(`${args[i].slice(2)} is ${args[i + 1]}`);
     }
   }
+
+  console.log(result.join(', '));
 };
 
 parseArgs();
