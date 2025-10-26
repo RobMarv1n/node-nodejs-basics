9346e87804f4b259f533603f2c216c5a802a3b03
Rob Marvin <fenalfan@gmail.com>
2025-10-26 23:55:59 +0300

    fix: fix import error

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,7 +1,7 @@
 import { createReadStream } from 'node:fs';
 import path from 'node:path';
 import { createHash } from 'node:crypto';
-import { errors } from '../constants/errors';
+import { errors } from '../constants/errors.js';
 
 const __dirname = import.meta.dirname;
 const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
