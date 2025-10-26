152e7f2aeec58942a4108ddaaf230c9bd3e816d0
Rob Marvin <fenalfan@gmail.com>
2025-10-26 12:51:21 +0300

    feat: implement write file function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import { createWriteStream } from 'node:fs';
+import path from 'node:path';
+import { errors } from '../constants/errors.js';
+
+const __dirname = import.meta.dirname;
+const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-  // Write your code here
+  try {
+    const stream = createWriteStream(filePath);
+    console.log('Enter some text. Press Ctrl+C to quit:\n');
+    process.stdin.pipe(stream);
+  } catch {
+    throw new Error(errors.fileWriteFailed);
+  }
 };
 
 await write();
