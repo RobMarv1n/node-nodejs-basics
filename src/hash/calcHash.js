1c0737d6bb0305961ef294070ef62797b728888e
Rob Marvin <fenalfan@gmail.com>
2025-10-26 09:13:25 +0300

    feat: implement calcHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { createReadStream } from 'node:fs';
+import path from 'node:path';
+import { createHash } from 'node:crypto';
+import { errors } from '../constants/errors';
+
+const __dirname = import.meta.dirname;
+const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-  // Write your code here
+  try {
+    const hash = createHash('sha256');
+    const stream = createReadStream(filePath);
+
+    for await (const chunk of stream) {
+      hash.update(chunk);
+    }
+
+    console.log(hash.digest('hex'));
+  } catch {
+    throw new Error(errors.hashCalculationFailed);
+  }
+
 };
 
 await calculateHash();
